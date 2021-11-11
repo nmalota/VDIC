b68d32f3fe1f2d5df88bdfe69789a08f984121d0
natalia malota <nataliamalota11@gmail.com>
2021-11-11 20:04:56 +0100

    fixes in gitignore

--- Lab0/INCA_libs_gui/run.lnx86.19.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1633614993 /home/student/nmalota/VDIC/Lab0/apple_tb.sv
-1633613624 /home/student/nmalota/VDIC/Lab0/apple.sv
--- Lab0/INCA_libs_gui/run.lnx86.19.09.d/files.ts
+++
@@ -1,3 +0,0 @@
-1552390470 /home/student/nmalota/cds.lib
-1633613624 /home/student/nmalota/VDIC/Lab0/apple.sv
-1633614993 /home/student/nmalota/VDIC/Lab0/apple_tb.sv
