fc67db147e868f2ab9edfdc7d82bc1ecda7c8987
natalia malota <nataliamalota11@gmail.com>
2021-12-02 23:48:30 +0100

    changed getdata in minmax tester

--- lab05/tb_classes/min_max_tester.svh
+++ lab05/tb_classes/min_max_tester.svh
@@ -3,14 +3,12 @@ class min_max_tester extends random_tester;
 	`uvm_component_utils(min_max_tester)
 
 	protected function bit [31:0] get_data();
-		bit [1:0] zero_ones;
-		zero_ones = 2'($random);
-		if (zero_ones == 2'b00)
+		bit zero_ones;
+		zero_ones = 1'($random);
+		if (zero_ones == 1'b0)
 			return 32'h00000000;
-		else if (zero_ones == 2'b11)
+		else 
 			return 32'hFFFFFFFF;
-		else
-			return 32'($random);
 	endfunction : get_data
 
 	function new (string name, uvm_component parent);
